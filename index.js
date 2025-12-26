3d7ff7621972f9ecf0008744aef2ac71b5da8a8a
vanbasten2234-cell <vanbasten2234@gmail.com>
2025-12-25 21:38:19 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,55 @@
-const {uptimeDate} = require("./uptime.js");
-const Discord = require("discord.js");
-const client = new Discord.Client({intents: 3276799});
-let prefix = "-";
-client.on("ready", () => {
-     console.log(`${client.user.tag} is ready!!`)
-})
+const Discord = require('discord.js-selfbot-v13');
+const client = new Discord.Client({
+  readyStatus: false,
+  checkUpdate: false
+});
+//environment
+require('dotenv').config()
 
+function formatTime() { 
+  const date = new Date();
+  const options = {
+    timeZone: 'America/New_York', 
+    hour12: true,
+    hour: 'numeric',
+    minute: 'numeric'
+  };
+  return new Intl.DateTimeFormat('en-US', options).format(date);
+}
+const express = require("express")
+const app = express();
+var listener = app.listen(process.env.PORT || 2000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+app.listen(() => console.log("I'm Ready To Work..! 24H"));
+app.get('/', (req, res) => {
+  res.send(`
+  <body>
+  <center><h1>Bot 24H ON!</h1></center
+  </body>`)
+});
+client.on('ready', async () => {
+  console.clear();
+  console.log(`${client.user.tag} - rich presence started!`);
 
-client.on("messageCreate", async (message) => {
-   if(!message.content || message.author.bot || !message.guild) return;
-   let args = message.content.trim().split(/ +/)
-   let cmd = args[0].slice(prefix.length).toLowerCase();
-  switch(cmd) {
-    case "uptime":
-      message.reply(`- **Uptime Since:** <t:${Math.floor(uptimeDate / 1000)}:R>`)
-      break;
-  }
-  
-  
-})
+  const r = new Discord.RichPresence()
+    .setApplicationId('356877880938070016')
+    .setType('PLAYING')
+    .setURL('https://www.rocketleague.com/en/play') 
+    .setState('PLAY')
+    .setName('roketleague')
+    .setDetails(`pkaying now`)
+    .setStartTimestamp(Date.now())
+ .setAssetsLargeImage('https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Rocket_League_coverart.jpg/960px-Rocket_League_coverart.jpg') //You can put links in tenor or discord and etc.
+    .setAssetsLargeText('welcome') 
+    .setAssetsSmallImage('https://chaosboost.com/wp-content/uploads/webpcache/2024/09/rocket-league-rank-boost.webp') //You can put links in tenor or discord and etc.
+    .setAssetsSmallText('come and play with me') 
+    .addButton('https://www.rocketleague.com/en/play');
 
-client.login(process.env.token)
+  client.user.setActivity(r);
+  client.user.setPresence({ status: "dnd" }); //dnd, online, idle, offline
+
+});
+
+const mySecret = process.env['TOKEN'];
+client.login(mySecret);
